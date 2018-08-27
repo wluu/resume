ad7b476b7929bea8a15814e83c71174a416c9971
Wilson Luu <luuwilson@gmail.com>
2018-08-26 20:31:12 -0700

    Correctly referencing the pdf file when running locally and pushed to production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  // the base url is implicit i.e. https://wluu.github.io/
+  pdfUrl: '/resume/assets/wilson_automation.pdf'
 };
